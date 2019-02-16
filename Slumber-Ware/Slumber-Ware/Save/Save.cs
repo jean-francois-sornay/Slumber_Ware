fe775d038cc02164e11f6d9dc5268a0528e7970b
jean-francois-sornay <jean-francois.sornay@etu.umontpellier.fr>
2019-02-16 21:49:31 +0100

    Creation de classe

---
+++ Slumber-Ware/Slumber-Ware/Save/Save.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace Slumber_Ware
+{
+    public class Save
+    {
+        public static void _save(int id)
+        {
+            StructurePerso p = new StructurePerso
+            {
+                Id = id,
+                LastName = "Dupond",
+                FirstName = "Jean",
+                Address = new Address
+                {
+                    Street = "1, rue du petit pont",
+                    ZipCode = "75005",
+                    City = "Paris",
+                    Country = "France"
+                }
+            };
+
+            XmlSerializer xs = new XmlSerializer(typeof(StructurePerso));
+            using (StreamWriter wr = new StreamWriter("DataPlayer.xml"))
+            {
+                xs.Serialize(wr, p);
+            }
+        }
+
+        public static StructurePerso _createObject()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(StructurePerso));
+            using (StreamReader rd = new StreamReader("DataPlayer.xml"))
+            {
+                StructurePerso p = xs.Deserialize(rd) as StructurePerso;
+                return p;
+            }
+        }
+
+
+    }
+}
---
+++ Slumber-Ware/Slumber-Ware/Save/StructurePerso.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slumber_Ware
+{
+    public class StructurePerso
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public Address Address { get; set; }
+
+        public override string ToString()
+        {
+            string res = "";
+            res += Id + "\n" + LastName + "\n" + FirstName + "\n" + Address + "\n";
+            return res;
+        }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            string res = "";
+            res += Street + "\n" + ZipCode + "\n" + City + "\n" + Country + "\n";
+            return res;
+        }
+    }
+}
